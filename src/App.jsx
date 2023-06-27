885141c6e5b8a71ef591015fab1636e61888fad1
junior-boop <54283084+junior-boop@users.noreply.github.com>
2023-06-27 16:01:02 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Footer from '../composants/footer';
 import Hygienes from '../pages/filieres/hyg_as';
 import Hydraulique from '../pages/filieres/hyd_ru';
 import Inscription from '../pages/inscription';
+import ProductionV from '../pages/filieres/prod_veg';
+import ProductionA from '../pages/filieres/prod_an';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Route path="/" component={Accueil} />
         <Route path="/filieres/hyg-as" component={Hygienes} />
         <Route path="/filieres/hyd-ru" component={Hydraulique} />
+        <Route path="/filieres/prod-veg" component={ProductionV} />
+        <Route path="/filieres/prod-an" component={ProductionA} />
         <Route path="/inscription" component={Inscription} />
       </Routes>
       <Footer />
